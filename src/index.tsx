9489178012428f905890f90be78187dc087c18b7
DaniloLima122 <limadanilo06@gmail.com>
2022-12-02 19:48:10 -0300

    feat: 🎸 it adds progressbar component

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,8 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
-
 import './global.style.scss';
 
-const rootElement = document.getElementById('root');
-const root = createRoot(rootElement);
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-root.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
-);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
